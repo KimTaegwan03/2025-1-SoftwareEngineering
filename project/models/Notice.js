4c9c5a7abc5f1cd7fb2cbf5390932c1f5795bdae
kimpro <kk2007pp@gmail.com>
2025-05-20 14:58:19 +0900

    Notice db allowNull:false 추가

--- project/models/Notice.js
+++ project/models/Notice.js
@@ -1,15 +1,37 @@
+const { DataTypes } = require('sequelize');
+const sequelize = require('../db');
+
 const Notice = sequelize.define('Notice', {
   id: {
     type: DataTypes.INTEGER,
     primaryKey: true,
     autoIncrement: true
   },
-  writer: DataTypes.STRING,
-  title: DataTypes.STRING,
-  content: DataTypes.TEXT,
-  reg_date: DataTypes.DATE,
-  up_date: DataTypes.DATE,
-  del_date: DataTypes.DATE
+  writer: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  title: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  content: {
+    type: DataTypes.TEXT,
+    allowNull: false
+  },
+  image: {
+    type: DataTypes.STRING, // image url
+  },
+  reg_date: {
+    type: DataTypes.DATE,
+    allowNull: false
+  },
+  up_date: {
+    type: DataTypes.DATE,
+  },
+  del_date: {
+    type: DataTypes.DATE,
+  },
 });
 
 module.exports = Notice;
