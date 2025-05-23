6f0438b38dda6dc04f3f3f1f2bd8ecb183fc5ccf
pjy <pjy0807111@naver.com>
2025-05-23 16:30:33 +0900

    merge completed

--- project/models/student.js
+++ project/models/student.js
@@ -2,21 +2,17 @@ const { DataTypes } = require('sequelize');
 const sequelize = require('../db');
 
 const Student = sequelize.define('Student', {
-    ID: {
-        type: DataTypes.INTEGER,
-        primaryKey: true,
-        autoIncrement: true
-    },
-    name: { type: DataTypes.STRING, allowNull: false },
+    ID : {  type: DataTypes.INTEGER,
+            primaryKey: true,
+            autoIncrement: true },
+    name : { type: DataTypes.STRING, allowNull: false },
     dept_name: { type: DataTypes.STRING, allowNull: false },
     tot_cred: { type: DataTypes.INTEGER, allowNull: false },
-    acc_id: { type: DataTypes.STRING, allowNull: false, unique: true }, // account id
+    acc_id: { type: DataTypes.STRING, allowNull: false , unique: true }, // account id
     password: { type: DataTypes.STRING, allowNull: false },
-    email: { type: DataTypes.STRING, allowNull: false },
-    phone: { type: DataTypes.STRING, allowNull: false },
-    address: { type: DataTypes.STRING, allowNull: false }
-}, {
-    timestamps: false
+    email : { type: DataTypes.STRING, allowNull: false },
+    phone : { type: DataTypes.STRING, allowNull: false },
+    address : { type: DataTypes.STRING, allowNull: false }
 });
 
 module.exports = Student;
